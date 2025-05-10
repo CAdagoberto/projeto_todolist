bce3908385fcf63de04d8421b8d6ee744dfb306e
Cristian Vianna <jbcristian78@gmail.com>
2025-05-10 16:50:01 -0300

    componentizando profileIcon

---
+++ src/components/profile/PerfilIcon.jsx
@@ -0,0 +1,9 @@
+import styles from './perfilIcon.module.css'
+
+export default function profileIcon() {
+    return (
+        <>
+            <div className={styles.profile}></div>
+        </>
+    )
+}
